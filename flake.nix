fa02db9b0e7a04518d7c4c9ceed617c0c81e0c13
Cass Chin <casschin@gmail.com>
2025-11-16 00:46:43 -0500

    add lazygit

--- flake.nix
+++ flake.nix
@@ -23,41 +23,23 @@
       nixpkgs.config.allowUnfree = true;
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages = [ 
+      environment.systemPackages = [
         pkgs._1password-gui
         pkgs.brave
         pkgs.ghostty-bin
         pkgs.google-chrome
-        pkgs.neovim
+        pkgs.lazygit
+        pkgs.nerd-fonts.iosevka
         pkgs.raycast
         pkgs.vscode
       ];
 
-      system.primaryUser = "cass";
-      system.defaults = {
-        dock.autohide = true;
-        dock.persistent-apps = [];
-        NSGlobalDomain.KeyRepeat = 2;
-        CustomUserPreferences = {
-          "com.apple.symbolichotkeys" = {
-            AppleSymbolicHotKeys = {
-              # Disable 'Cmd + Space' for Spotlight Search
-              "64" = {
-                enabled = false;
-              };
-            };
-          };
-        };
-      };
-      system.keyboard = {
-      	enableKeyMapping = true;
-  	    remapCapsLockToControl = true;
-      };
 
       homebrew = {
         enable = true;
         brews = [
           "mas"
+	        "mise"
         ];
 
         # only packages in this config will be installed, otherwise, others will be removed
@@ -71,15 +53,25 @@
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+      system = {
+        # Set Git commit hash for darwin-version.
+        configurationRevision = self.rev or self.dirtyRev or null;
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        stateVersion = 6;
+        primaryUser = "cass";
+        defaults = {
+          dock.autohide = true;
+          dock.persistent-apps = [];
+          NSGlobalDomain = {
+            KeyRepeat = 2;
+          };
+        };
+      };
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
+      fonts.packages = [
+        pkgs.nerd-fonts.iosevka
+      ];
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
